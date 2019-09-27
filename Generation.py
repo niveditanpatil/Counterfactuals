9e0750e8e07a5421e666f82572826e2b998a605f
Nivedita Patil <npatil@rdc.com>
2019-09-27 14:58:09 -0400

    rearranged code added main

--- Generation.py
+++ Generation.py
@@ -8,14 +8,6 @@ import numpy as np
 # ### Steps 1-5 Algorithm 1
 # <tt>observation_to_interpret</tt> is nothing but the point whose counterfactual we are trying to find. <tt>sample_space</tt> is the min and max limits of the features as described in the dataset.
 
-filename = 'binary_classifier.sav'
-binary_classifier = pickle.load(open(filename, 'rb'))
-observation_to_interpret = [ 1.  , 90.  , 62.  , 12.  , 43.  , 27.2 ,  0.58, 24.  ] #this is just the first row of the test set. its result is 0
-sample_space = [[0,17] , [0,199] , [0,122] , [0,99] , [0,846] , [0,67.1] , [0.08,2.42] , [21,81]]
-radius_eta = 5 #hyperparameters
-observations_n = 10 #hyperparameters
-main_result = binary_classifier.predict([observation_to_interpret])
-
 
 # We need to find the closest 'enemy' e i.e. an observation whose result is the opposite of the observation to interpret and is closest to it vector wise
 
@@ -59,54 +51,61 @@ def start_looking(z,eta):
             return eta/2
     return
 
-eta = radius_eta
-z = make_z(eta,0,eta)
 
-while start_looking(z,eta)!=None:
-    eta = start_looking(z,eta)
-    old_z = z
+if __name__ == '__main__':
+    filename = 'binary_classifier.sav'
+    binary_classifier = pickle.load(open(filename, 'rb'))
+    observation_to_interpret = [ 1.  , 90.  , 62.  , 12.  , 43.  , 27.2 ,  0.58, 24.  ] #this is just the first row of the test set. its result is 0
+    sample_space = [[0,17] , [0,199] , [0,122] , [0,99] , [0,846] , [0,67.1] , [0.08,2.42] , [21,81]]
+    radius_eta = 5 #hyperparameters
+    observations_n = 10 #hyperparameters
+    main_result = binary_classifier.predict([observation_to_interpret])
+
+
+    eta = radius_eta
     z = make_z(eta,0,eta)
-print(eta)
-print(old_z)
+
+    while start_looking(z,eta)!=None:
+        eta = start_looking(z,eta)
+        old_z = z
+        z = make_z(eta,0,eta)
 
 
-# ### Steps 6-12 Algorithm 1
+    # ### Steps 6-12 Algorithm 1
 
-a0 = eta
-a1 = 2*eta
+    a0 = eta
+    a1 = 2*eta
 
-n_eta = radius_eta
-o_z = z
-while 1:
-    if not 1 in [binary_classifier.predict([i])[0] for i in z]:
-        o_z = z
-        z = make_z(a1,a0,a1)
-        a0=a1
-        a1=a1+eta
-    else:
-        break
-print(o_z)
+    n_eta = radius_eta
+    o_z = z
+    while 1:
+        if not 1 in [binary_classifier.predict([i])[0] for i in z]:
+            o_z = z
+            z = make_z(a1,a0,a1)
+            a0=a1
+            a1=a1+eta
+        else:
+            break
+    # print('')
 
-enemy = z[[binary_classifier.predict([i])[0] for i in z].index(1)]
+    enemy = z[[binary_classifier.predict([i])[0] for i in z].index(1)]
 
 
-# ### Steps 1-7 Algorithm 2
+    # ### Steps 1-7 Algorithm 2
 
-get_indices = lambda x, xs: [i for (y, i) in zip(xs, range(len(xs))) if x == y]
+    get_indices = lambda x, xs: [i for (y, i) in zip(xs, range(len(xs))) if x == y]
 
-enemy_prime = enemy
+    enemy_prime = enemy
 
-while binary_classifier.predict([enemy_prime])!= main_result:
-    enemy_star = enemy_prime
-    to_minimize = []
-    for j in range(len(enemy_prime)):
-        if enemy_prime[j]!= observation_to_interpret[j]:
-            to_minimize.append(np.absolute(enemy_prime[j]-observation_to_interpret[j]))
-    to_change = get_indices(min(to_minimize),to_minimize)
-    for i in to_change:
-        enemy_prime[i] = observation_to_interpret[i]
-        
-print(enemy_star)
+    while binary_classifier.predict([enemy_prime])!= main_result:
+        enemy_star = enemy_prime
+        to_minimize = []
+        for j in range(len(enemy_prime)):
+            if enemy_prime[j]!= observation_to_interpret[j]:
+                to_minimize.append(np.absolute(enemy_prime[j]-observation_to_interpret[j]))
+        to_change = get_indices(min(to_minimize),to_minimize)
+        for i in to_change:
+            enemy_prime[i] = observation_to_interpret[i]
 
-print('Enemy:\t\t'+str(enemy_star))
-print('Original:\t'+str(observation_to_interpret))
+    print('Enemy:\t\t'+str(enemy_star))
+    print('Original:\t'+str(observation_to_interpret))
